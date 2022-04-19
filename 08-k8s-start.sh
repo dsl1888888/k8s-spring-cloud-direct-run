1cf8ec0d46abb4defe3490b8b2eb5608bc141c73
dsl1888888 <dsl1888888@163.com>
2022-04-20 02:06:27 +0800

    add

--- 08-k8s-start.sh
+++ 08-k8s-start.sh
@@ -2,7 +2,7 @@
 #起跑服务(有顺序的,先中心,再启动其他;你的文件放在哪里,你就指定你的路径).
 echo "起跑服务(有顺序的,先中心,再启动其他)."
 
-microk8s.kubectl apply -f /root/k8s-run-spring-cloud-parent/dockerBuildAndRun/docker-k8s-eureka/k8s-eureka-server.yaml
-microk8s.kubectl apply -f /root/k8s-run-spring-cloud-parent/dockerBuildAndRun/docker-k8s-app01/k8s-app01.yaml
-microk8s.kubectl apply -f /root/k8s-run-spring-cloud-parent/dockerBuildAndRun/docker-k8s-app02/k8s-app02.yaml
-microk8s.kubectl apply -f /root/k8s-run-spring-cloud-parent/dockerBuildAndRun/docker-k8s-zuul/k8s-api-gateway.yaml
+microk8s.kubectl apply -f /opt/k8s-spring-cloud-direct-run/docker-k8s-eureka/k8s-eureka-server.yaml
+microk8s.kubectl apply -f /opt/k8s-spring-cloud-direct-run/docker-k8s-app01/k8s-app01.yaml
+microk8s.kubectl apply -f /opt/k8s-spring-cloud-direct-run/docker-k8s-app02/k8s-app02.yaml
+microk8s.kubectl apply -f /opt/k8s-spring-cloud-direct-run/docker-k8s-zuul/k8s-api-gateway.yaml
